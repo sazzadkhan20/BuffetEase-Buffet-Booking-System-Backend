f848039ba4c7a829dabfd77a050795fa96f42b4d
sazzadkhan20 <sazzad53913@gmail.com>
2026-01-22 13:00:27 +0600

    Initialize BuffetEase Web API Project

---
+++ src/main/java/com/BuffetEase/BuffetEaseApiApplication.java
@@ -0,0 +1,13 @@
+package com.BuffetEase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BuffetEaseApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BuffetEaseApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/BuffetEase/BuffetEaseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.BuffetEase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BuffetEaseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
